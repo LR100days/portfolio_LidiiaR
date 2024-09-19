55a36615d0916ab15b6f9765e51a80bb81fb7b02
LR100days <160536109+LR100days@users.noreply.github.com>
2024-09-19 13:56:03 +0200

    Fix typo in search.py test, add new test add_product_to_the_cart (#2)

--- UI_tests/add_product_to_the_cart.py
+++ UI_tests/add_product_to_the_cart.py
@@ -3,7 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-URL = "replaceme"
+URL = "replace"
 
 # To keep the browser window opened
 chrome_options = webdriver.ChromeOptions()
@@ -13,16 +13,22 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(URL)
 
-#Search for a product and check if matching products are found
-search_key = "berry" # will find several results, that contain this search key
+#Search for a products
+search_key = "berry" # will find 2 results, that contain this search key
 driver.find_element(By.CLASS_NAME, "search-keyword").send_keys(search_key)
 driver.find_element(By.CLASS_NAME, "search-button").click()
 time.sleep(2)
 results = driver.find_elements(By.TAG_NAME, "h4")
-count = len(results)
-assert count == 2
 for item in results:
     assert search_key in item.text
+
+# Add the products to the cart
+products = driver.find_elements(By.XPATH, "//div[@class='products']/div")
+for item in products:
+    item.find_element(By.XPATH, "div/button").click()
+
+added_items = driver.find_element(By.XPATH, "//div[@class='cart-info']/table/tbody/tr[1]/td[3]/strong")
+assert added_items.text == "2"
 
 # End program execution
 driver.quit()
